32fe2434d9323ca48f14a09606bc79fac5a045da
Sambhav Gupta <sambhavgupta@microsoft.com>
2021-07-04 07:22:18 +0530

    first commit

---
+++ src/main/java/com/microsoft/springbootchatapp/ChatController.java
@@ -0,0 +1,24 @@
+package com.microsoft.springbootchatapp;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessagePojo sendMessage(@Payload ChatMessagePojo chatMessagePojo) {
+        return chatMessagePojo;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessagePojo addUser(@Payload ChatMessagePojo chatMessagePojo, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessagePojo.getSender());
+        return chatMessagePojo;
+    }
+}
---
+++ src/main/java/com/microsoft/springbootchatapp/SpringBootChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft.springbootchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatAppApplication.class, args);
+	}
+
+}
